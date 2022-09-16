3d10d14fddc365837b448facd90ce1069c7cbbed
LucasRocha <lucasrochacv1@gmail.com>
2022-09-16 16:37:41 -0300

    correcao de detalhes geral

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,5 @@
 const background2 = document.getElementById('backgroundAnimation-two')
 const shoot = document.getElementById('shootBk')
-
 shoot.addEventListener('mouseenter', () => { 
     background2.style.display = 'block'
 })
@@ -10,9 +9,12 @@ shoot.addEventListener('mouseout', () => {
 
 function nav() {
     let lista = document.getElementById('lista')
+    let t = document.getElementById('t')
     if(lista.style.display == '' || lista.style.display == 'none'){
         lista.style.display = 'flex'
+        t.classList.toggle('animate')
     }else{
         lista.style.display = 'none'
+        t.classList.remove('animate')
     }
 }
