39b9943d5c30ea164c6c1fd3c08326130d2e60e4
LucasRocha <lucasrochacv1@gmail.com>
2022-09-15 23:12:14 -0300

    alteracoes no header

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,14 +1,18 @@
-const background = document.getElementById('backgroundAnimation')
 const background2 = document.getElementById('backgroundAnimation-two')
 const shoot = document.getElementById('shootBk')
 
 shoot.addEventListener('mouseenter', () => { 
     background2.style.display = 'block'
-    // background.classList.toggle('two')
-    console.log(background2.style.display)
 })
 shoot.addEventListener('mouseout', () => {
     background2.style.display = 'none'
-    // background.classList.remove('two')
-
 })
+
+function nav() {
+    let lista = document.getElementById('lista')
+    if(lista.style.display == '' || lista.style.display == 'none'){
+        lista.style.display = 'flex'
+    }else{
+        lista.style.display = 'none'
+    }
+}
